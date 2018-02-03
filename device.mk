01948d08b0d5494b96cd4cf192294e8ed98e835e
Wei Wang <wvw@google.com>
2018-02-02 21:32:05 -0800

    laurel_sprout: Remove QTI perfd
    Bug: 69270928
    Test: Build and boot device and test camera
    Change-Id: I87b283206f462fb5c8ec9cdd303ae2934fe9bfc4
    Signed-off-by: Noctowl709 <beastdark704@gmail.com>

--- device.mk
+++ device.mk
@@ -402,8 +402,7 @@ PRODUCT_COPY_FILES += \
 
 # Power
 PRODUCT_PACKAGES += \
-    android.hardware.power@1.3-service.laurel_sprout-libperfmgr \
-    vendor.qti.hardware.perf@2.2.vendor
+    android.hardware.power@1.3-service.laurel_sprout-libperfmgr
 
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/perf/powerhint.json:$(TARGET_COPY_OUT_VENDOR)/etc/powerhint.json \
