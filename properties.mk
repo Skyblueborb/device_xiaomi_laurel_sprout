67e176a69637f7fd8f331bc83ec133c96102fd21
Woody Lin <woodylin@google.com>
2020-10-28 11:22:19 +0800

    laurel_sprout: Enable zygote critical window
    Bug: 141948707
    Change-Id: I5dc57312118b0f6d95fc9fe164363ec7e15f1b51
    Signed-off-by: Pascoato <viniciuspascoato@gmail.com>
    Signed-off-by: anirudhkosgi <kosgi34@gmail.com>

--- properties.mk
+++ properties.mk
@@ -273,4 +273,5 @@ PRODUCT_SYSTEM_DEFAULT_PROPERTIES += \
 
 # Zygote
 PRODUCT_SYSTEM_DEFAULT_PROPERTIES += \
+	zygote.critical_window.minute=10 \
     persist.device_config.runtime_native.usap_pool_enabled=true
