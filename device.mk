1f6ef3cf4eb62d62b8caea887cf5474a9313c2a0
Vishalcj17 <vishalcj@aospa.co>
2020-12-22 11:42:38 +0530

    laurel: Setup Infrared Sensor.
    - From qssi-user-10-QKQ1.190910.002-V11.0.23.0.QFQMIXM-release-keys.
    
    Change-Id: I365f2fa055ddd3af7edc23e65446a1a4a4f170bf

--- device.mk
+++ device.mk
@@ -193,6 +193,14 @@ PRODUCT_PACKAGES += \
     init.qti.dcvs.sh \
     init.target.rc
 
+# IR
+PRODUCT_PACKAGES += \
+    android.hardware.ir@1.0-impl \
+    android.hardware.ir@1.0-service
+
+PRODUCT_COPY_FILES += \
+    frameworks/native/data/etc/android.hardware.consumerir.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.consumerir.xml
+
 # Keymaster
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.strongbox_keystore.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.strongbox_keystore.xml
