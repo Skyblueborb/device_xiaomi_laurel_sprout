36f81a70db1067aab983d40a2dfa9d16fad31742
Vishalcj17 <vishalcj@aospa.co>
2021-01-04 22:26:45 +0530

    Revert "laurel: Add protobuf vendorcompat lib."
    - No need as the new camera stack fulfills protobuf needs.
    
    This reverts commit a57fa245dc6a6904839046921933fef37c4d9265.
    
    Change-Id: Idc07307b6164586c5cc1a8e7a04f0ab3b4d3c7df

--- device.mk
+++ device.mk
@@ -245,11 +245,6 @@ PRODUCT_SYSTEM_EXT_PROPERTIES += \
     ro.sensor.proximity=true \
     ro.sensor.pickup=android.sensor.tilt_detector
 
-# Protobuf
-PRODUCT_PACKAGES += \
-    libprotobuf-cpp-full-vendorcompat \
-    libprotobuf-cpp-lite-vendorcompat
-
 # Public Libraries
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/public.libraries.txt:$(TARGET_COPY_OUT_VENDOR)/etc/public.libraries.txt
