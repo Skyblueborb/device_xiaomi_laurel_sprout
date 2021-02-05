a9a47477dce630e4c3a48d352c6af064b78d3743
Vishalcj17 <vishalcj@aospa.co>
2021-02-05 13:33:45 +0530

    laurel: Don't ship audio_platform_info.
    - We don't need it anymore.
    
    Change-Id: I8200f8f939a250f83d44ca227cd5cac76ff44a83

--- device.mk
+++ device.mk
@@ -77,7 +77,6 @@ PRODUCT_PROPERTY_OVERRIDES += \
     vendor.audio.feature.spkr_prot.enable=false
 
 PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/configs/audio/audio_platform_info_qrd.xml:$(TARGET_COPY_OUT_VENDOR)/etc/audio/audio_platform_info_qrd.xml \
     $(LOCAL_PATH)/configs/audio/audio_policy_configuration.xml:$(TARGET_COPY_OUT_VENDOR)/etc/audio/audio_policy_configuration.xml \
     $(LOCAL_PATH)/configs/audio/audio_policy_volumes.xml:$(TARGET_COPY_OUT_VENDOR)/etc/audio_policy_volumes.xml \
     $(LOCAL_PATH)/configs/audio/default_volume_tables.xml:$(TARGET_COPY_OUT_VENDOR)/etc/default_volume_tables.xml \
