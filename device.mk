0a61bf794a000ae491257dd1a0a43e905241c224
Sebastiano Barezzi <barezzisebastiano@gmail.com>
2021-02-08 12:30:57 +0100

    laurel_sprout: Import public libraries list
    * From laurel_sprout-user 11 RKQ1.200903.002 V12.0.26.0.RFQMIXM release-keys
    
    Change-Id: I41725fac378391291e84cca00c16b1661797e22b

--- device.mk
+++ device.mk
@@ -232,6 +232,10 @@ PRODUCT_COPY_FILES += \
 PRODUCT_PACKAGES += \
     android.hardware.power-service-qti
 
+# Public libraries
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/configs/public.libraries.txt:$(TARGET_COPY_OUT_VENDOR)/etc/public.libraries.txt
+
 # QMI
 PRODUCT_PACKAGES += \
     libjson \
