6c0824b8f0cb7ac86095148abce2fe9e235f1a46
Sebastiano Barezzi <barezzisebastiano@gmail.com>
2021-02-08 08:37:49 +0100

    laurel_sprout: Add arch and bootloader flags
    Change-Id: I578f01a20d604fd8c936cffbeed83b967906f638

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -6,6 +6,25 @@
 
 DEVICE_PATH := device/xiaomi/laurel_sprout
 
+# Architecture
+TARGET_ARCH := arm64
+TARGET_ARCH_VARIANT := armv8-a
+TARGET_CPU_ABI := arm64-v8a
+TARGET_CPU_ABI2 :=
+TARGET_CPU_VARIANT := generic
+TARGET_CPU_VARIANT_RUNTIME := cortex-a73
+
+TARGET_2ND_ARCH := arm
+TARGET_2ND_ARCH_VARIANT := armv8-a
+TARGET_2ND_CPU_ABI := armeabi-v7a
+TARGET_2ND_CPU_ABI2 := armeabi
+TARGET_2ND_CPU_VARIANT := generic
+TARGET_2ND_CPU_VARIANT_RUNTIME := cortex-a73
+
+# Bootloader
+TARGET_BOOTLOADER_BOARD_NAME := trinket
+TARGET_NO_BOOTLOADER := true
+
 # Display
 TARGET_SCREEN_DENSITY := 320
 
