eafc8eff4540796b92fc0f3f7de6ac84d2f0c1df
Sebastiano Barezzi <barezzisebastiano@gmail.com>
2021-02-08 14:48:21 +0100

    laurel_sprout: Import wifi configs
    * From laurel_sprout-user 11 RKQ1.200903.002 V12.0.26.0.RFQMIXM release-keys
    
    Change-Id: I363ddda7a99bd261930300dec147f2b86d909727
    Signed-off-by: Skyblueborb <tomaszborbely0710@gmail.com>

--- device.mk
+++ device.mk
@@ -323,6 +323,11 @@ PRODUCT_PACKAGES += \
     wpa_supplicant \
     wpa_supplicant.conf
 
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/configs/wifi/p2p_supplicant_overlay.conf:$(TARGET_COPY_OUT_VENDOR)/etc/wifi/p2p_supplicant_overlay.conf \
+    $(LOCAL_PATH)/configs/wifi/wpa_supplicant_overlay.conf:$(TARGET_COPY_OUT_VENDOR)/etc/wifi/wpa_supplicant_overlay.conf \
+    $(LOCAL_PATH)/configs/wifi/WCNSS_qcom_cfg.ini:$(TARGET_COPY_OUT_VENDOR)/etc/wifi/WCNSS_qcom_cfg.ini
+
 # WiFi Display
 PRODUCT_PACKAGES += \
     libnl \
