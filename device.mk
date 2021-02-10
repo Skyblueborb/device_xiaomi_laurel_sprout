743effff913e9826d40f3abaf1ee8805aa89332d
Sebastiano Barezzi <barezzisebastiano@gmail.com>
2021-02-10 22:10:39 +0100

    laurel_sprout: Build WFD libs
    Change-Id: I859cd3ff8e1729323f2c72df4feca55559262878

--- device.mk
+++ device.mk
@@ -234,5 +234,13 @@ PRODUCT_PACKAGES += \
     wpa_supplicant \
     wpa_supplicant.conf
 
+# WiFi Display
+PRODUCT_PACKAGES += \
+    libnl \
+    libwfdaac_vendor
+
+PRODUCT_BOOT_JARS += \
+    WfdCommon
+
 # Inherit the proprietary files
 $(call inherit-product, vendor/xiaomi/laurel_sprout/laurel_sprout-vendor.mk)
