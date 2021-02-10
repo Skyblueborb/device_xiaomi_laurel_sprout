d1baaa234f368fcb591de85f0bac35ae45498a6e
Sebastiano Barezzi <barezzisebastiano@gmail.com>
2021-02-10 22:06:43 +0100

    laurel_sprout: Build RIL libs
    Change-Id: I449475abffebdf837e5645e77b571e0efd9988a1

--- device.mk
+++ device.mk
@@ -187,6 +187,12 @@ PRODUCT_PACKAGES += \
     libvndfwk_detect_jni.qti \
     libvndfwk_detect_jni.qti.vendor
 
+# RIL
+PRODUCT_PACKAGES += \
+    libprotobuf-cpp-full \
+    librmnetctl \
+    libxml2
+
 # Shipping API level
 PRODUCT_SHIPPING_API_LEVEL := 28
 
