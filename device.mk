c666c4e37dbbe15a703d7cfced6ff4df97d7c5b8
Vishalcj17 <vishalcj@aospa.co>
2021-02-10 17:47:32 +0530

    Revert "laurel: Use phase offsets as duration."
    - SurfaceFlinger Durations work fine in some scenarios like during fling, browsing on chrome and etc.
      But it breaks HWUI graph during activities like scrolling or JankBench tests,
      even when the the governor is set to performance.
    
    This reverts commit 59656345eb55e4ec87868cb7ede50be765e8210c.
    
    Change-Id: Ib8370ea1f0dc9e43947095c42d45c9b8ec293cd8

--- device.mk
+++ device.mk
@@ -164,15 +164,6 @@ PRODUCT_PACKAGES += \
     libqservice \
     libtinyxml
 
-PRODUCT_PROPERTY_OVERRIDES += \
-    debug.sf.use_phase_offsets_as_durations=1 \
-    debug.sf.late.sf.duration=10500000 \
-    debug.sf.late.app.duration=20500000 \
-    debug.sf.early.sf.duration=21000000 \
-    debug.sf.early.app.duration=16500000 \
-    debug.sf.earlyGl.sf.duration=13500000 \
-    debug.sf.earlyGl.app.duration=21000000
-
 # GPS
 LOC_HIDL_VERSION = 4.0
 
