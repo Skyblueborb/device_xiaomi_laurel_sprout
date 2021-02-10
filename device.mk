081f29fd7ab920ac86ff6d33be33417ab269d9c0
Sebastiano Barezzi <barezzisebastiano@gmail.com>
2021-02-10 22:09:03 +0100

    laurel_sprout: Build USB HIDL
    Change-Id: Id7da15ac872967e4fb733836c99efaf39aca3a56

--- device.mk
+++ device.mk
@@ -213,5 +213,9 @@ PRODUCT_PACKAGES += \
 PRODUCT_BOOT_JARS += \
     telephony-ext
 
+# USB
+PRODUCT_PACKAGES += \
+    android.hardware.usb@1.0-service
+
 # Inherit the proprietary files
 $(call inherit-product, vendor/xiaomi/laurel_sprout/laurel_sprout-vendor.mk)
