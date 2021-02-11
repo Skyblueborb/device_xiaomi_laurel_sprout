a024050aa53bca580cea40dc4eb0bdfea9643dc9
Sebastiano Barezzi <barezzisebastiano@gmail.com>
2021-02-11 11:02:49 +0100

    laurel_sprout: Initial lineage-sdk overlays
    * Define hardware keys
    
    Change-Id: I9d2728eeeef5c72488824670d2a4c2bde5db7d51
    Signed-off-by: Skyblueborb <tomaszborbely0710@gmail.com>

--- device.mk
+++ device.mk
@@ -185,7 +185,8 @@ PRODUCT_PACKAGES += \
 
 # Overlays
 DEVICE_PACKAGE_OVERLAYS += \
-    $(LOCAL_PATH)/overlay
+    $(LOCAL_PATH)/overlay \
+    $(LOCAL_PATH)/overlay-lineage
 
 PRODUCT_ENFORCE_RRO_TARGETS := *
 
