afcedb8c1e2439c59893f3258dd248a629b86296
Vishalcj17 <vishalcj@aospa.co>
2021-03-12 15:56:18 +0530

    laurel: Move compatibility_matrix to QCOM common.
    Change-Id: I5fcc6c6efa509c4c4553697ba6c5bb3b130f43ce

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -78,7 +78,7 @@ DEVICE_MANIFEST_FILE := \
     $(DEVICE_PATH)/manifest.xml \
     $(DEVICE_PATH)/xiaomi_manifest.xml
 
-DEVICE_MATRIX_FILE := $(DEVICE_PATH)/compatibility_matrix.xml
+DEVICE_MATRIX_FILE += device/qcom/common/compatibility_matrix.xml
 
 # Kernel
 BOARD_KERNEL_CMDLINE := \
