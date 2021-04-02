f52c5cd6dfb388d19013baaac0bce9e0324abf3b
Vishalcj17 <vishalcj@aospa.co>
2021-04-02 19:13:31 +0530

    laurel: Import WLAN Supplicant overlays.
    - From LA.UM.9.11.r1-03200-NICOBAR.0.
    
    Change-Id: Ibc716eaa8fd29adedcc717b2bb7db9603a031a6d

--- device.mk
+++ device.mk
@@ -295,4 +295,6 @@ $(call inherit-product, vendor/xiaomi/laurel_sprout/laurel_sprout-vendor.mk)
 
 # Wlan
 PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/configs/wlan/WCNSS_qcom_cfg.ini:$(TARGET_COPY_OUT_VENDOR)/etc/wifi/WCNSS_qcom_cfg.ini
+    $(LOCAL_PATH)/configs/wlan/WCNSS_qcom_cfg.ini:$(TARGET_COPY_OUT_VENDOR)/etc/wifi/WCNSS_qcom_cfg.ini \
+    $(LOCAL_PATH)/configs/wlan/p2p_supplicant_overlay.conf:$(TARGET_COPY_OUT_VENDOR)/etc/wifi/p2p_supplicant_overlay.conf \
+    $(LOCAL_PATH)/configs/wlan/wpa_supplicant_overlay.conf:$(TARGET_COPY_OUT_VENDOR)/etc/wifi/wpa_supplicant_overlay.conf
