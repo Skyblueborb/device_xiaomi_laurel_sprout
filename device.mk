97d3bef05bd2f99b9678b2855a4016357f32fc2e
Adithya R <gh0strider.2k18.reborn@gmail.com>
2021-04-04 15:23:12 +0300

    laurel_sprout: Switch back to MiuiCamera
    Welcome back

--- device.mk
+++ device.mk
@@ -175,6 +175,8 @@ PRODUCT_PACKAGES += \
     vendor.qti.hardware.btconfigstore@2.0.vendor
 
 # Camera
+$(call inherit-product, vendor/miuicamera/config.mk)
+
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.camera.flash-autofocus.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.camera.flash-autofocus.xml \
     frameworks/native/data/etc/android.hardware.camera.front.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.camera.front.xml \
