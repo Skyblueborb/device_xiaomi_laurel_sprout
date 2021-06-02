5456d608445e6c97958d462cf591e04b1388bd6b
Sebastiano Barezzi <barezzisebastiano@gmail.com>
2021-06-02 17:58:37 +0200

    laurel_sprout: Build LiveDisplay HIDL
    Change-Id: I6b767fe2269b679438c8fc0c49ceef9110a96a23

--- device.mk
+++ device.mk
@@ -153,6 +153,10 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     lights.trinket
 
+# LiveDisplay
+PRODUCT_PACKAGES += \
+    vendor.lineage.livedisplay@2.0-service-sdm
+
 # Media
 PRODUCT_PACKAGES += \
     libavservices_minijail \
