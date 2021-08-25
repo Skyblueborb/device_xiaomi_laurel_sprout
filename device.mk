b8c376210d531cedce235a8b681f6cd1dd85e732
Magicxavi <rolfpeterkesche@gmail.com>
2021-08-25 13:14:04 +0300

    laurel_sprout: Drop QCOM WFD

--- device.mk
+++ device.mk
@@ -502,13 +502,13 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/wifi/WCNSS_qcom_cfg.ini:$(TARGET_COPY_OUT_VENDOR)/etc/wifi/WCNSS_qcom_cfg.ini
 
 # WiFi Display
-PRODUCT_PACKAGES += \
-    libaacwrapper \
-    libnl \
-    libwfdaac_vendor
+#PRODUCT_PACKAGES += \
+#    libaacwrapper \
+#    libnl \
+#    libwfdaac_vendor
 
-PRODUCT_BOOT_JARS += \
-    WfdCommon
+#PRODUCT_BOOT_JARS += \
+#    WfdCommon
 
 # Inherit proprietary files
 $(call inherit-product-if-exists, vendor/xiaomi/laurel_sprout/laurel_sprout-vendor.mk)
