f0d5389a282082bc119f9d44bc3150419aff4153
Joker-V2 <nipinna0@gmail.com>
2021-11-06 18:10:11 +0000

    laurel_sprout: Disable hwc vds
    Signed-off-by: Skyblueborb <tomaszborbely0710@gmail.com>

--- properties.mk
+++ properties.mk
@@ -155,7 +155,6 @@ PRODUCT_PROPERTY_OVERRIDES += \
     debug.sf.early_gl_app_phase_offset_ns=15000000 \
     debug.sf.early_gl_phase_offset_ns=3000000 \
     debug.sf.early_phase_offset_ns=1500000 \
-    debug.sf.enable_hwc_vds=1 \
     debug.sf.hw=1 \
 	debug.sf.auto_latch_unsignaled=true \
 	debug.sf.enable_gl_backpressure=1 \
