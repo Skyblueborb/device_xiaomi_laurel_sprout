e463c6dbe7a7887903468eb79bd225167b64bc8c
Andrzej Perczak <linux@andrzejperczak.com>
2021-12-02 15:55:13 +0100

    laurel_sprout: Don't report memcg functionality in cmdline
    Memcg was disabled in kernel as on Google devices, it is said
    to cause performance regression.
    
    Signed-off-by: Andrzej Perczak <linux@andrzejperczak.com>
    Change-Id: I3f1670394666eb6527c045e7e22acaea6d8c71e9
    Signed-off-by: Richard Raya <rdxzv.dev@gmail.com>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -86,7 +86,6 @@ BOARD_MKBOOTIMG_ARGS += --header_version $(BOARD_BOOTIMG_HEADER_VERSION)
 BOARD_KERNEL_CMDLINE := console=null
 BOARD_KERNEL_CMDLINE += androidboot.hardware=qcom
 BOARD_KERNEL_CMDLINE += androidboot.console=ttyMSM0
-BOARD_KERNEL_CMDLINE += androidboot.memcg=1
 BOARD_KERNEL_CMDLINE += lpm_levels.sleep_disabled=1
 BOARD_KERNEL_CMDLINE += video=vfb:640x400,bpp=32,memsize=3072000
 BOARD_KERNEL_CMDLINE += msm_rtb.filter=0x237
