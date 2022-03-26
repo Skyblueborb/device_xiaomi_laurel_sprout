c6ad8d30d41a8adc66441c212e2b6afa43436653
iHemal <i.mominulislamhemal@gmail.com>
2022-03-26 08:57:35 +0000

    laurel_sprout: Force volte
    volte goes brrrrrr

--- device.mk
+++ device.mk
@@ -196,3 +196,10 @@ PRODUCT_PACKAGES += \
 
 PRODUCT_PACKAGES_DEBUG += \
     update_engine_client
+
+# Volte
+PRODUCT_PRODUCT_PROPERTIES += \
+    persist.dbg.ims_volte_enable=1 \
+    persist.dbg.volte_avail_ovr=1 \
+    persist.dbg.wfc_avail_ovr=1 \
+    persist.dbg.vt_avail_ovr=1
