5a31f04958ddc4ab0b9651aacfb6dafbcfc7ad58
Skyblueborb <tomaszborbely0710@gmail.com>
2022-04-17 16:21:58 +0000

    BoardConfig.mk: Switch to proton-clang for kernel compilation

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -89,7 +89,7 @@ TARGET_KERNEL_CLANG_COMPILE := true
 TARGET_KERNEL_CONFIG := vendor/laurel_sprout-perf_defconfig
 TARGET_KERNEL_SOURCE := kernel/xiaomi/laurel_sprout
 TARGET_KERNEL_NO_LLVM_BINUTILS := true
-TARGET_KERNEL_CLANG_VERSION:= r437113
+TARGET_KERNEL_CLANG_VERSION:= proton 
 
 # Platform
 TARGET_BOARD_PLATFORM := trinket
