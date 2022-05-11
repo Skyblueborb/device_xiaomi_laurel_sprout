26309350e517e9683165a4312857d0933c586524
Hemant Sachdeva <hemant.evolver@gmail.com>
2022-05-11 10:17:50 +0300

    laurel_sprout: Build libwpa_client

--- device.mk
+++ device.mk
@@ -484,6 +484,7 @@ PRODUCT_PACKAGES += \
     android.hardware.wifi@1.0-service \
     hostapd \
     libwifi-hal-qcom \
+    libwpa_client \
     wcnss_service \
     WifiOverlay \
     wpa_supplicant \
