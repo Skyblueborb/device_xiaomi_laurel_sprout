a9e99c8cdb81c0a36cccdfe2cf408368f6a9e9b0
Magicxavi <rolfpeterkesche@gmail.com>
2022-05-11 13:37:42 +0300

    laurel_sprout: Switch to Pixel power-libperfmgr HAL

--- device.mk
+++ device.mk
@@ -363,7 +363,8 @@ PRODUCT_COPY_FILES += \
 
 # Power
 PRODUCT_PACKAGES += \
-    android.hardware.power-service-qti \
+    android.hardware.power@1.3-service.laurel_sprout-libperfmgr \
+    android.hardware.power.stats@1.0-service.pixel \
     vendor.qti.hardware.perf@2.2.vendor
 
 PRODUCT_COPY_FILES += \
@@ -427,7 +428,8 @@ PRODUCT_VENDOR_PROPERTIES += \
 # Soong namespaces
 PRODUCT_SOONG_NAMESPACES += \
     $(LOCAL_PATH) \
-    hardware/xiaomi
+    hardware/xiaomi\
+    hardware/google/interfaces
 
 QCOM_SOONG_NAMESPACE := $(LOCAL_PATH)
 
