7e0d8b15a5598b902adef522acb8abf2716773cd
Nolen Johnson <johnsonnolen@gmail.com>
2022-05-11 10:04:41 +0300

    laurel_sprout: Simplify AVB vbmeta flags logic

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -187,8 +187,7 @@ BOARD_VNDK_VERSION := current
 
 # Verified Boot
 BOARD_AVB_ENABLE := true
-BOARD_AVB_MAKE_VBMETA_IMAGE_ARGS += --set_hashtree_disabled_flag
-BOARD_AVB_MAKE_VBMETA_IMAGE_ARGS += --flags 2
+BOARD_AVB_MAKE_VBMETA_IMAGE_ARGS += --flags 3
 
 # WiFi
 BOARD_WLAN_DEVICE := qcwcn
