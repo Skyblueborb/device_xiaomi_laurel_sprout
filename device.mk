118bbb5d0a2bf39a1e332ad051936c60a78e8ec9
Magicxavi <rolfpeterkesche@gmail.com>
2022-05-13 06:44:48 +0300

    laurel_sprout: Use gesture navigation by default

--- device.mk
+++ device.mk
@@ -320,6 +320,10 @@ PRODUCT_ENFORCE_RRO_TARGETS := *
 PRODUCT_ENFORCE_RRO_EXCLUDED_OVERLAYS += \
     $(LOCAL_PATH)/overlay/packages/apps/CarrierConfig
 
+# Use gesture navigation by default
+PRODUCT_PRODUCT_PROPERTIES += \
+    ro.boot.vendor.overlay.theme=com.android.internal.systemui.navbar.gestural
+
 # Permissions
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/system-privapp-permissions-qti.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/permissions/privapp-permissions-qti.xml \
