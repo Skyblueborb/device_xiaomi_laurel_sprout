0b8f92a55166e23c06bc9a8ccefe94ab09c83cc7
Ady Abraham <adyabr@google.com>
2022-07-11 18:04:33 +0700

    laurel_sprout: Enable GL comp backpressure
    Enable backpressure for client composition to avoid jank due to
    HWC queue stuffing.
    
    Test: 1. Open Camera
          2. Swipe horizontally to go back to previous app.
    
    Bug: 138083790
    Change-Id: I3d26aa470c1660b23bd97133f39858f92638aca8
    Signed-off-by: anirudhkosgi <kosgi34@gmail.com>

--- properties.mk
+++ properties.mk
@@ -157,6 +157,7 @@ PRODUCT_PROPERTY_OVERRIDES += \
     debug.sf.enable_hwc_vds=1 \
     debug.sf.hw=1 \
     debug.sf.latch_unsignaled=1 \
+	debug.sf.enable_gl_backpressure=1 \
     ro.opengles.version=196610 \
     ro.hardware.egl=adreno \
     ro.hardware.vulkan=adreno \
