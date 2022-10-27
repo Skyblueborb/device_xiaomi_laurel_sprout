1d686eb57f81d29f6c87cb59375bd90a755d0af3
dev-harsh1998 <god@hyper-labs.tech>
2022-10-28 00:15:14 +0300

    laurel_sprout: Compile HWUI for performance
    * Activates few optimizing compiler flags for HWUI.
    * -fno-omit-frame-pointer, -marm, -mapcs
    
    Change-Id: Ia1391b1a6cbfd876459042f88e58f2eafa17d9d6

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -99,6 +99,9 @@ DEVICE_FRAMEWORK_MANIFEST_FILE := $(DEVICE_PATH)/configs/manifests/framework_man
 DEVICE_MANIFEST_FILE := $(DEVICE_PATH)/configs/manifests/manifest.xml
 DEVICE_MATRIX_FILE := $(DEVICE_PATH)/configs/manifests/compatibility_matrix.xml
 
+# HWUI
+HWUI_COMPILE_FOR_PERF := true
+
 # Init
 TARGET_INIT_VENDOR_LIB := //$(DEVICE_PATH):libinit_trinket
 TARGET_RECOVERY_DEVICE_MODULES := libinit_trinket
