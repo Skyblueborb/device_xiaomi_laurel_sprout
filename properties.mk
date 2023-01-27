afa377d8157d38b469e8920a9a97cc79b8b34fc0
Jabiyeff <cebiyevanar@gmail.com>
2023-01-27 09:23:26 +0800

    laurel_sprout: clean up useless log spams
    Signed-off-by: clarencelol <clarencekuiek@icloud.com>
    Signed-off-by: Skyblueborb <tomaszborbely0710@gmail.com>

--- properties.mk
+++ properties.mk
@@ -209,6 +209,20 @@ PRODUCT_PROPERTY_OVERRIDES += \
 PRODUCT_PROPERTY_OVERRIDES += \
     ro.hardware.keystore_desede=true
 
+# Logs
+PRODUCT_SYSTEM_DEFAULT_PROPERTIES += \
+	persist.log.tag.AnalyticsService=S \
+	persist.log.tag.KernelCpuUidActiveTimeReader=S \
+	persist.log.tag.Tracer=S \
+	persist.log.tag.NearbySharing=S \
+	persist.log.tag.IntervalStats=S \
+	persist.log.tag.CompatibilityChangeReporter=S \
+	persist.log.tag.SQLiteLog=S \
+	persist.log.tag.wificond=S \
+	persist.log.tag.b/223498680=S \
+	persist.log.tag.TrafficStats=S \
+	persist.log.tag.ContrastColorUtil=S
+
 # Media
 PRODUCT_PROPERTY_OVERRIDES += \
     debug.stagefright.ccodec=1 \
