bd8e478163c04950f6d2bcc84ff6b2c9940a8821
FrankFFFF <kweifat@gmail.com>
2023-04-30 16:21:57 +0800

    init_trinket: correct heapgrowthlimit of 4GB variant
    Tidy it up too while I am here, 
    I might have some use later, and this was hideous.
    
    Also, there's no 2GB variant of Laurel.
    
    Signed-off-by: Fieah Lim <kweifat@gmail.com>

--- init/init_trinket.cpp
+++ init/init_trinket.cpp
@@ -5,15 +5,15 @@
    Redistribution and use in source and binary forms, with or without
    modification, are permitted provided that the following conditions are
    met:
-    * Redistributions of source code must retain the above copyright
-      notice, this list of conditions and the following disclaimer.
-    * Redistributions in binary form must reproduce the above
-      copyright notice, this list of conditions and the following
-      disclaimer in the documentation and/or other materials provided
-      with the distribution.
-    * Neither the name of The Linux Foundation nor the names of its
-      contributors may be used to endorse or promote products derived
-      from this software without specific prior written permission.
+	* Redistributions of source code must retain the above copyright
+	  notice, this list of conditions and the following disclaimer.
+	* Redistributions in binary form must reproduce the above
+	  copyright notice, this list of conditions and the following
+	  disclaimer in the documentation and/or other materials provided
+	  with the distribution.
+	* Neither the name of The Linux Foundation nor the names of its
+	  contributors may be used to endorse or promote products derived
+	  from this software without specific prior written permission.
 
    THIS SOFTWARE IS PROVIDED "AS IS" AND ANY EXPRESS OR IMPLIED
    WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF
@@ -42,67 +42,58 @@
 #include "vendor_init.h"
 #include "property_service.h"
 
-char const *heapstartsize;
-char const *heapgrowthlimit;
-char const *heapsize;
-char const *heapminfree;
-char const *heapmaxfree;
-char const *heaptargetutilization;
-
-void check_device()
+static void property_override(char const prop[], char const value[], bool add = true)
 {
-    struct sysinfo sys;
+	auto pi = (prop_info *) __system_property_find(prop);
 
-    sysinfo(&sys);
-
-    if (sys.totalram > 5072ull * 1024 * 1024) {
-        // from - phone-xhdpi-6144-dalvik-heap.mk
-        heapstartsize = "16m";
-        heapgrowthlimit = "256m";
-        heapsize = "512m";
-        heaptargetutilization = "0.5";
-        heapminfree = "8m";
-        heapmaxfree = "32m";
-    } else if (sys.totalram > 3072ull * 1024 * 1024) {
-        // from - phone-xxhdpi-4096-dalvik-heap.mk
-        heapstartsize = "8m";
-        heapgrowthlimit = "256m";
-        heapsize = "512m";
-        heaptargetutilization = "0.6";
-        heapminfree = "8m";
-        heapmaxfree = "16m";
-    } else {
-        // from - phone-xhdpi-2048-dalvik-heap.mk
-        heapstartsize = "8m";
-        heapgrowthlimit = "192m";
-        heapsize = "512m";
-        heaptargetutilization = "0.75";
-        heapminfree = "512k";
-        heapmaxfree = "8m";
-    }
+	if (pi != nullptr) {
+		__system_property_update(pi, value, strlen(value));
+	} else if (add) {
+		__system_property_add(prop, strlen(prop), value, strlen(value));
+	}
 }
 
-void property_override(char const prop[], char const value[], bool add = true)
+static void set_dalvik_heap()
 {
-    auto pi = (prop_info *) __system_property_find(prop);
+	struct sysinfo sys;
+	char const *heapstartsize;
+	char const *heapgrowthlimit;
+	char const *heapsize;
+	char const *heapminfree;
+	char const *heapmaxfree;
+	char const *heaptargetutilization;
 
-    if (pi != nullptr) {
-        __system_property_update(pi, value, strlen(value));
-    } else if (add) {
-        __system_property_add(prop, strlen(prop), value, strlen(value));
-    }
+	sysinfo(&sys);
+
+	if (sys.totalram > 5072ull * 1024 * 1024) {
+		// from - phone-xhdpi-6144-dalvik-heap.mk
+		heapstartsize = "16m";
+		heapgrowthlimit = "256m";
+		heapsize = "512m";
+		heaptargetutilization = "0.5";
+		heapminfree = "8m";
+		heapmaxfree = "32m";
+	} else if (sys.totalram > 3072ull * 1024 * 1024) {
+		// from - phone-xxhdpi-4096-dalvik-heap.mk
+		heapstartsize = "8m";
+		heapgrowthlimit = "192m";
+		heapsize = "512m";
+		heaptargetutilization = "0.6";
+		heapminfree = "8m";
+		heapmaxfree = "16m";
+	}
+
+	property_override("dalvik.vm.heapstartsize", heapstartsize);
+	property_override("dalvik.vm.heapgrowthlimit", heapgrowthlimit);
+	property_override("dalvik.vm.heapsize", heapsize);
+	property_override("dalvik.vm.heaptargetutilization", heaptargetutilization);
+	property_override("dalvik.vm.heapminfree", heapminfree);
+	property_override("dalvik.vm.heapmaxfree", heapmaxfree);
 }
 
 void vendor_load_properties()
 {
-    check_device();
+	set_dalvik_heap();
 
-    property_override("dalvik.vm.heapstartsize", heapstartsize);
-    property_override("dalvik.vm.heapgrowthlimit", heapgrowthlimit);
-    property_override("dalvik.vm.heapsize", heapsize);
-    property_override("dalvik.vm.heaptargetutilization", heaptargetutilization);
-    property_override("dalvik.vm.heapminfree", heapminfree);
-    property_override("dalvik.vm.heapmaxfree", heapmaxfree);
-
-    vendor_load_device_properties();
+	vendor_load_device_properties();
 }
