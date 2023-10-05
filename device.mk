f9bb4ce2a56235c70fda2bd61239ce04217bd805
zafeey <archfeey@gmail.com>
2023-10-06 06:43:15 +0800

    laurel_sprout: Clean up camera stuff

--- device.mk
+++ device.mk
@@ -90,7 +90,9 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     libcamera2ndk_vendor \
     libdng_sdk.vendor \
-    libgui_vendor
+    libgui_vendor \
+    libstdc++.vendor \
+    vendor.qti.hardware.camera.device@1.0.vendor
 
 # Charger
 PRODUCT_PACKAGES += \
