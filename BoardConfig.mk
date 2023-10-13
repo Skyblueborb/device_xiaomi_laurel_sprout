c97d59303b5d463c2607ec455a3ad034087207c8
Magicxavi <rolfpeterkesche@gmail.com>
2023-10-13 18:09:42 +0300

    laurel_sprout: update manifests
    Signed-off-by: Skyblueborb <tomaszborbely0710@gmail.com>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -97,6 +97,7 @@ LOC_HIDL_VERSION := 4.0
 DEVICE_FRAMEWORK_MANIFEST_FILE := $(DEVICE_PATH)/configs/manifests/framework_manifest.xml
 DEVICE_MANIFEST_FILE := $(DEVICE_PATH)/configs/manifests/manifest.xml
 DEVICE_MATRIX_FILE := $(DEVICE_PATH)/configs/manifests/compatibility_matrix.xml
+DEVICE_FRAMEWORK_COMPATIBILITY_MATRIX_FILE += $(DEVICE_PATH)/configs/manifests/framework_compatibility_matrix.xml
 
 # HWUI
 HWUI_COMPILE_FOR_PERF := true
