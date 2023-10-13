31f8b802bb6034607bf5c41558bd5cadbbfdba94
Pavel Dubrova <pashadubrova@gmail.com>
2023-10-13 20:32:46 +0300

    laurel_sprout: Provide android.frameworks.sensorservice@1.0 on vendor
    QCamera : <MCI><ERROR> mm_camera_load_shim_lib: 3306: dlopen failed
        with error dlopen failed: library
        "android.frameworks.sensorservice@1.0.so" not found: needed by
        /odm/lib/libmmcamera2_stats_modules.so in namespace (default)
    
    android.frameworks.sensorservice@1.0 is used by at least mm-camera
    framework.
    
    Needed by slim_daemon (gps daemon as well in U)
    10-08 09:41:12.189  1473  1473 F linker  : CANNOT LINK EXECUTABLE "slim_daemon": library "android.frameworks.sensorservice@1.0.so" not found: needed by main executable
    
    Signed-off-by: adi8900 <adrianszymanski242@gmail.com>
    Signed-off-by: Skyblueborb <tomaszborbely0710@gmail.com>

--- device.mk
+++ device.mk
@@ -443,9 +443,10 @@ PRODUCT_COPY_FILES += \
 
 # Sensors
 PRODUCT_PACKAGES += \
-    android.hardware.sensors@2.0-service.multihal \
+    android.frameworks.sensorservice@1.0.vendor \
+    android.hardware.sensors@2.1-service.multihal \
     libsensorndkbridge \
-    android.hardware.sensors@2.0.vendor
+    android.hardware.sensors@2.1.vendor
 
 # Shim
 PRODUCT_PACKAGES += \
