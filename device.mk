1351f635330422286d9e164f8ea7aa56269f9141
LuK1337 <priv.luk@gmail.com>
2023-10-20 00:53:15 +0200

    laurel_sprout: Build libprotobuf-cpp-lite-3.9.1-vendorcompat
    Change-Id: I0aac742db9c2b4b3e48ee20158f19405863893c2
    Signed-off-by: Skyblueborb <tomaszborbely0710@gmail.com>

--- device.mk
+++ device.mk
@@ -416,7 +416,8 @@ PRODUCT_PACKAGES += \
 # Protobuf
 PRODUCT_PACKAGES += \
     libprotobuf-cpp-full \
-    libprotobuf-cpp-full-3.9.1-vendorcompat
+    libprotobuf-cpp-full-3.9.1-vendorcompat \
+    libprotobuf-cpp-lite-3.9.1-vendorcompat \
 
 # RenderScript HAL
 PRODUCT_PACKAGES += \
