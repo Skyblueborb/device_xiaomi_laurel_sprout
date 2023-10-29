80a5b28f54993706e6770593acc0adc7a3163bc7
Noctowl709 <beastdark704@gmail.com>
2023-10-29 10:23:07 +0530

    laurel_sprout: light: Rebrand light hal to laurel_sprout
    Signed-off-by: Noctowl709 <beastdark704@gmail.com>

--- light/service.cpp
+++ light/service.cpp
@@ -5,7 +5,7 @@
  * SPDX-License-Identifier: Apache-2.0
  */
 
-#define LOG_TAG "android.hardware.light@2.0-service.xiaomi_trinket"
+#define LOG_TAG "android.hardware.light@2.0-service.laurel_sprout"
 
 #include <android-base/logging.h>
 #include <hidl/HidlTransportSupport.h>
