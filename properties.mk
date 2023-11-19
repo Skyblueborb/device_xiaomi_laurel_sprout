2f389cbd0dbdcc9f3135149dd0eb827d6682c600
Skyblueborb <tomaszborbely0710@gmail.com>
2023-11-19 17:25:20 +0100

    laurel_sprout: some LTE_CA and VoLTE stuff
    Signed-off-by: Skyblueborb <tomaszborbely0710@gmail.com>

--- properties.mk
+++ properties.mk
@@ -236,7 +236,8 @@ PRODUCT_PROPERTY_OVERRIDES += \
     persist.vendor.radio.procedure_bytes=SKIP \
     persist.vendor.radio.rat_on=combine \
     persist.vendor.radio.sib16_support=1 \
-    ro.telephony.iwlan_operation_mode=legacy
+    ro.telephony.iwlan_operation_mode=legacy \
+    persist.sys.radio.force_lte_ca = true
 
 PRODUCT_SYSTEM_DEFAULT_PROPERTIES += \
     DEVICE_PROVISIONED=1 \
