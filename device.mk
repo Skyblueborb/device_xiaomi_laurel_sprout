750dd10bf68b886f48f1c82578c37c581b5fc4bc
Manish Kushwaha <kushwahamanish@google.com>
2023-12-07 01:30:46 +0800

    laurel_sprout: Migrate sensor HAL to AIDL interface
    Bug: 247478991
    Test: tested on-device (P23) ab/P46941077
    Test: `sensor_test -ahal list` and `sensor_test list`
    Test: `sensor_test -ahal sample -s0` and `sensor_test sample -s0`
    
    Change-Id: I40f6ab6b7780c53cd638a1f77c95f6c324dc2d15
    Signed-off-by: ralph950412 <ralph950412@gmail.com>
    Signed-off-by: Skyblueborb <tomaszborbely0710@gmail.com>

--- device.mk
+++ device.mk
@@ -380,7 +380,7 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     android.frameworks.sensorservice@1.0 \
     android.frameworks.sensorservice@1.0.vendor \
-    android.hardware.sensors@2.1-service.xiaomi-multihal
+    android.hardware.sensors-service.xiaomi-multihal
 
 PRODUCT_PACKAGES += \
     libsensorndkbridge
