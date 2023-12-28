8264c3b0641ff8e1b998fa4a452c77beb4a3143b
Skyblueborb <tomaszborbely0710@gmail.com>
2023-12-28 17:00:33 +0100

    laurel_sprout: build android.hidl.base and libprotobuf-cpp-lite-3.9.1-vendorcompat
    Signed-off-by: Skyblueborb <tomaszborbely0710@gmail.com>

--- device.mk
+++ device.mk
@@ -239,6 +239,7 @@ endif
 
 # HIDL
 PRODUCT_PACKAGES += \
+    android.hidl.base@1.0 \
     android.hidl.base@1.0.vendor \
     android.hidl.manager@1.0.vendor \
     android.hidl.base@1.0_system \
@@ -415,7 +416,7 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     libprotobuf-cpp-full \
     libprotobuf-cpp-full-3.9.1-vendorcompat \
-    libprotobuf-cpp-lite-3.9.1-vendorcompat \
+    libprotobuf-cpp-lite-3.9.1-vendorcompat
 
 # RenderScript HAL
 PRODUCT_PACKAGES += \
