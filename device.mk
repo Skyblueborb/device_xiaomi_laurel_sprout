bfa7e9bb4b28c930cacec3f8c25d6dc3482f39c4
Magicxavi <rolfpeterkesche@gmail.com>
2024-01-23 09:56:28 +0200

    laurel_sprout: build system android.frameworks.sensorservice

--- device.mk
+++ device.mk
@@ -447,6 +447,7 @@ PRODUCT_COPY_FILES += \
 
 # Sensors
 PRODUCT_PACKAGES += \
+    android.frameworks.sensorservice@1.0 \
     android.frameworks.sensorservice@1.0.vendor \
     android.hardware.sensors@2.1-service.multihal \
     libsensorndkbridge \
