77916880395a9206ddde0868c1bdb84ef74aa368
Skyblueborb <tomaszborbely0710@gmail.com>
2024-02-06 12:03:37 +0100

    laurel_sprout: move back to OSS camera stack
    Revert "laurel_sprout: move to stock camera stack for now"
    
    This reverts commit 27ca6f4188c14990a038b756e45cf031a2371dd3.
    
    Signed-off-by: Skyblueborb <tomaszborbely0710@gmail.com>
    
    camera tmp
    
    Signed-off-by: Skyblueborb <tomaszborbely0710@gmail.com>

--- device.mk
+++ device.mk
@@ -103,7 +103,13 @@ PRODUCT_PACKAGES += \
 
 # Camera
 PRODUCT_PACKAGES += \
+    android.frameworks.displayservice@1.0.vendor \
+    android.hardware.camera.provider@2.4-impl \
+    android.hardware.camera.provider@2.4-service \
+    vendor.qti.hardware.camera.device@1.0.vendor \
     libstdc++_vendor \
+    libgui_vendor \
+    libcamera2ndk_vendor \
     libdng_sdk.vendor
 
 PRODUCT_COPY_FILES += \
