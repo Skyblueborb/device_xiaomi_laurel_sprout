3a193cb4197d2d9fb3b592331b3e61c21cab12e9
Skyblueborb <tomaszborbely0710@gmail.com>
2024-04-12 17:46:20 +0200

    laurel_sprout: use opensource LDAC
    Signed-off-by: Skyblueborb <tomaszborbely0710@gmail.com>

--- device.mk
+++ device.mk
@@ -144,6 +144,7 @@ PRODUCT_PACKAGES += \
     BluetoothQti \
     libbthost_if \
     libldacBT_dec \
+    libldacBT_abr \
     vendor.qti.hardware.bluetooth_audio@2.0.vendor \
     vendor.qti.hardware.btconfigstore@1.0.vendor \
     vendor.qti.hardware.btconfigstore@2.0.vendor
