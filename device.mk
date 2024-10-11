8c30f2e0ca3a1ab5e151a3111e753f89761e83b7
Skyblueborb <tomaszborbely0710@gmail.com>
2024-10-11 19:11:49 +0200

    laurel_sprout: use Android.mk for libhidlbase-v32
    This allows us to drop BUILD_BROKEN_ELF_PREBUILT_PRODUCT_COPY_FILES as
    it was the only breaking change.
    
    Ref: https://review.lineageos.org/c/LineageOS/android_device_samsung_gta4xl-common/+/338869
    
    Signed-off-by: Skyblueborb <tomaszborbely0710@gmail.com>

--- device.mk
+++ device.mk
@@ -406,9 +406,8 @@ PRODUCT_PACKAGES += \
     android.hardware.vibrator@1.0-service
 
 # VNDK
-PRODUCT_COPY_FILES += \
-    prebuilts/vndk/v32/arm64/arch-arm-armv8-a/shared/vndk-sp/libhidlbase.so:$(TARGET_COPY_OUT_VENDOR)/lib/libhidlbase-v32.so \
-    prebuilts/vndk/v32/arm64/arch-arm64-armv8-a/shared/vndk-sp/libhidlbase.so:$(TARGET_COPY_OUT_VENDOR)/lib64/libhidlbase-v32.so
+PRODUCT_PACKAGES += \
+    libhidlbase-v32
 
 # WiFi
 PRODUCT_PACKAGES += \
