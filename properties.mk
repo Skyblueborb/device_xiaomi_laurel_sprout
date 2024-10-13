e467471ae1aa740e0465d4e6a0f45411d373c65a
Skyblueborb <tomaszborbely0710@gmail.com>
2024-10-13 17:07:10 +0200

    Revert "sm6125-common: enable unspecialized app process pool"
    This reverts commit 709686ff13f1a8b2284bf63a49aa197326d1facd.
    
    Signed-off-by: Skyblueborb <tomaszborbely0710@gmail.com>

--- properties.mk
+++ properties.mk
@@ -281,8 +281,3 @@ PRODUCT_PROPERTY_OVERRIDES += \
 # WFD
 PRODUCT_SYSTEM_DEFAULT_PROPERTIES += \
     persist.debug.wfd.enable=1
-
-# Zygote
-PRODUCT_SYSTEM_DEFAULT_PROPERTIES += \
-    persist.device_config.runtime_native.usap_pool_enabled=true
-
